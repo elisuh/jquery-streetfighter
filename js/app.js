50ac88e9e9bf9b5a628cec17747ece7242788de3
elisuh <elisaverna@gmail.com>
2016-03-06 18:39:11 -0500

    hadouken animate

--- js/app.js
+++ js/app.js
@@ -1,21 +1,27 @@
 $(document).ready(function() {
-	$('.ryu').mouseenter(function() {
-		$('.ryu-still').hide();
-		$('.ryu-ready').show();
-	})
-	.mouseleave(function() {
-		$('.ryu-ready').hide();
-		$('.ryu-still').show();
-	})
-	.mousedown(function() {
-		// play hadouken sound
-		$('.ryu-ready').hide();
-		$('.ryu-throwing').show();
-		$('.hadouken').show();
-		// animate hadouken to the right of screen
-	})
-	.mouseup(function() {
-		$('.ryu-throwing').hide();
-		$('.ryu-ready').show();
-	});
+  $('.ryu').mouseenter(function() {
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+  .mouseleave(function() {
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+  })
+  .mousedown(function() {
+    // play hadouken sound
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').show().animate(
+    	{'left': '1020px'},
+    	500,
+    	function() {
+    		$(this).hide();
+    		$(this).css('left', '520px');
+    	}
+    );
+  })
+  .mouseup(function() {
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
+  });
 });
