1d665fe2ed4e26b1f53fd943323a960be5e7d504
elisuh <elisaverna@gmail.com>
2016-03-07 19:22:59 -0500

    keydown edit 2

--- js/app.js
+++ js/app.js
@@ -23,11 +23,12 @@ $(document).ready(function(){
         $('.ryu-throwing').hide();
         $('.ryu-ready').show();
     })
-    .keydown(function(key){
-  if (key.keyCode == '88') {
-      $('.ryu-still').hide();
-      $('.ryu-cool').show();
- }
+  $(document).on('keydown', function(key) {
+    
+    if (key.keyCode === 88) {
+      $('.ryu-still').toggle();
+      $('.ryu-ready').toggle();
+    }
 });
 
 });
